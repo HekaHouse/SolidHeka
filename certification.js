4b0a98ae4d659f0c59a396e2eefbfa0728bc42a2
Aron Price <aron23@gmail.com>
2017-05-02 17:15:11 -0400

    verifying certification

--- certification.js
+++ certification.js
@@ -2,11 +2,11 @@ var forge = require('node-forge');
 var pki = forge.pki;
 
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 function str2ab(str) {
   var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint8Array(buf);
+  var bufView = new Uint16Array(buf);
   for (var i=0, strLen=str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
